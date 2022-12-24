6fff44cecd9f09d9a64e5ec75921b3bea4365520
Tushar Sharma <ts17995@gmail.com>
2022-12-24 22:00:11 +0530

    feat: make vue as optional peer dependency (#3)
    Upstream change from https://github.com/IanVS/prettier-plugin-sort-imports/pull/45

--- src/preprocessors/vue-preprocessor.ts
+++ src/preprocessors/vue-preprocessor.ts
@@ -1,11 +1,20 @@
-import { parse } from '@vue/compiler-sfc';
+import type { PrettierOptions } from '../types';
 
-import { PrettierOptions } from '../types';
 import { preprocessor } from './preprocessor';
 
 export function vuePreprocessor(code: string, options: PrettierOptions) {
-    const { descriptor } = parse(code);
-    const content = (descriptor.script ?? descriptor.scriptSetup)?.content ?? code;
+    try {
+        const { parse } = require('@vue/compiler-sfc');
+        const { descriptor } = parse(code);
+        const content = (descriptor.script ?? descriptor.scriptSetup)?.content ?? code;
 
-    return code.replace(content, `\n${preprocessor(content, options)}\n`);
+        return code.replace(content, `\n${preprocessor(content, options)}\n`);
+    } catch (err) {
+        if ((err as NodeJS.ErrnoException).code === 'MODULE_NOT_FOUND') {
+            console.warn(
+                '[@serverless-guru/prettier-plugin-import-order]: Could not process .vue file.  Please be sure that "@vue/compiler-sfc" is installed in your project.',
+            );
+            throw err;
+        }
+    }
 }
